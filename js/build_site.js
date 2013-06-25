86d4d493476910175827a99b395f6a9993c73882
OpenShift guest <sebeeadam@gmail.com>
2013-06-24 23:12:10 -0400

    fixing the Google Analytics Tracking across pages

--- js/build_site.js
+++ js/build_site.js
@@ -7,6 +7,7 @@
 function buildSiteFrame() {
     buildContentHolder();
     buildSiteMenu();
+    googleAnalyticsCall();
 }
 
 function buildContentHolder() {
@@ -40,4 +41,14 @@ function getCurrentPage() {
     var currentPage = currentPath.substring(currentPath.lastIndexOf('/') + 1,
                                             currentPath.lastIndexOf('.'));
     return currentPage;
+}
+
+function googleAnalyticsCall() {
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      
+      ga('create', 'UA-41614628-1', 'c9.io');
+      ga('send', 'pageview');
 }
